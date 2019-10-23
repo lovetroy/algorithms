d41b949be540a0cd21769d761c8bec43e1ac5641
lovetroy <2013778368@qq.com>
2019-10-24 07:41:01 +0800

    add check

--- code/valid-anagram-242.py
+++ code/valid-anagram-242.py
@@ -7,16 +7,21 @@ def is_anagram(s, t):
 def is_anagram(s, t):
     if len(s) != len(t):
         return False
-    return str2map(s) == str2map(t)
+    dic1, dic2 = [0] * 26, [0] * 26
+    for item in s:
+        dic1[ord(item) - ord('a')] += 1
+    for item in t:
+        dic2[ord(item) - ord('a')] += 1
+    return dic1 == dic2
 
 
-def str2map(string):
-    d = {}
-    for s in string:
-        if s not in d:
-            d[s] = 1
-        else:
-            d[s] += 1
-    return d
-
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    dic1, dic2 = {}, {}
+    for item in s:
+        dic1[item] = dic1.get(item, 0) + 1
+    for item in t:
+        dic2[item] = dic2.get(item, 0) + 1
+    return dic1 == dic2
 # print(is_anagram('ab', 'ba'))
