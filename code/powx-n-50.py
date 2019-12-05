5b888672dbadcc4b99529a5ca4112ec0663962eb
lovetroy <2013778368@qq.com>
2019-12-06 07:15:16 +0800

    pow

--- code/powx-n-50.py
+++ code/powx-n-50.py
@@ -8,24 +8,43 @@ def my_pow(x, n):
     return ans
 
 
-# def my_pow(x, n):
-#     if n == 1:
-#         return x
-#     if n < 0:
-#         return 1 / my_pow(x, -n)
-#     # if n % 2:
-#     #     return x * my_pow(x, n - 1)
-#     return my_pow(x * x, n / 2)
-#
-#
-# def my_pow(x, n):
-#     if n < 0:
-#         x = 1 / x
-#         n = -n
-#     while n > 1:
-#         x *= x
-#         n /= 2
-#     return x
+def my_pow(x, n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return x
+    if n < 0:
+        return 1 / my_pow(x, -n)
+    if n % 2:
+        return x * my_pow(x, n - 1)
+    return my_pow(x * x, n / 2)
+
+
+def my_pow(x, n):
+    if n < 0:
+        x = 1 / x
+        n = -n
+    ans = 1
+    while n > 0:
+        if n & 1:
+            ans *= x
+        x *= x
+        n >>= 1
+    return ans
+
+
+def my_pow(x, n):
+    if n < 0:
+        x = 1 / x
+        n = -n
+    ans = 1
+    current_product = x
+    while n > 0:
+        if n % 2:
+            ans = ans * current_product
+        current_product = current_product * current_product
+        n = int(2 / n)
+    return ans
 
 
 print(my_pow(2, 3))
