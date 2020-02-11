2e819a3b90f80aaeb5f82f4332473f6e61c251f3
lovetroy <2013778368@qq.com>
2020-02-11 09:04:40 +0800

    change num

--- code/111-minimum-depth-of-binary-tree.py
+++ code/111-minimum-depth-of-binary-tree.py
@@ -5,9 +5,9 @@ def min_depth(root):
     if not root:
         return 0
     if not root.left:
-        return min_depth(root.right)
+        return min_depth(root.right) + 1
     if not root.right:
-        return min_depth(root.left)
+        return min_depth(root.left) + 1
     return min(min_depth(root.left), min_depth(root.right)) + 1
 
 
